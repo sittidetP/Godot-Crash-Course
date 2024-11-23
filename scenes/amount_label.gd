974cc8589cb89864527b801c780799f9664081a4
sittidetP <pawutinan_s@silpakorn.edu>
2024-11-23 22:25:45 +0700

    add score label

---
+++ scenes/amount_label.gd
@@ -0,0 +1,12 @@
+extends Label
+
+var total_collectables: int = 0
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	var current := total_collectables - get_tree().get_node_count_in_group("collectables")
+	
+	text = "{current} / {total}".format({"current": current,"total": total_collectables})
+
+func _on_get_total_timer_timeout() -> void:
+	total_collectables = get_tree().get_node_count_in_group("collectables")
